7696da14845dbd949e3584676ba5303736b80e9c
Roman Kozhemyakin <offmailno5@gmail.com>
2021-10-19 22:31:26 +0300

    Add project files.

---
+++ FlaviusJosephus.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlaviusJosephusTask
+{
+    public static class FlaviusJosephus
+    {
+        public static int Solve(int playersNumber, int number)
+        {
+            if (playersNumber <= 0)
+            {
+                throw new ArgumentException($"{nameof(playersNumber)} can not be less or equal 0");
+            }
+
+            if (number <= 0)
+            {
+                throw new ArgumentException($"{nameof(number)} can not be less or equal 0");
+            }
+
+            var players = new LinkedList<int>();
+            for (int i = 0; i < playersNumber; i++)
+            {
+                players.AddLast(i);
+            }
+
+            LinkedListNode<int> current = null;
+            while (players.Count != 1)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    if (current == null)
+                    {
+                        current = players.First;
+                    }
+                    else
+                    {
+                        current = current.Next;
+                    }                    
+                }
+
+                if (current == null)
+                {
+                    current = players.First;
+                }
+
+                var prev = current.Previous;
+                Console.WriteLine(current.Value);
+                players.Remove(current);
+                current = prev;
+            }
+
+            return players.Last.Value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlaviusJosephusTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FlaviusJosephus.Solve(3, 3));
+        }
+    }
+}
